d8bfff9bc7c09172cc01902c9a54a0bb76b7585f
Irina <iradanilova035@gmail.com>
2024-04-29 10:23:00 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PZ7.DANILOVA.PR-23.106"
+include(":app")
+ 
